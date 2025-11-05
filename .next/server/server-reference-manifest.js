6fff60ac52605cd45d2828c1dff48d2721ee34da
Prakashsktx <prakashsk.tx@gmail.com>
2025-11-05 15:37:07 +0530

    outfit fixed

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"krKrhzB410j5uxHpOXhFzNF/pbiug5/CvMB8R2ebx0I=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
